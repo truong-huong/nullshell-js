c47b4af9143e6d0dd418ba0ffc14560ffe3d750e
truong-huong <46587494+truong-huong@users.noreply.github.com>
2019-09-02 00:22:04 -0700

    Update version.js

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 // Free Version
-var sversion = { "present":"1.0.0",
-                "update" : " " } ;
+var sversion = { "present":"1.0.0 SS",
+                "update" : " 1.2.0 SS" } ;
 // Premium Version
-var pversion = { "present":"1.0.0" ,
+var pversion = { "present":"1.0.0 PS" ,
                  "update":" " }
