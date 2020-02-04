cce005494a842fe8f98df4c46ca2bbbb94c60705
truong-huong <46587494+truong-huong@users.noreply.github.com>
2020-02-04 11:40:47 +0700

    Delete version.js

--- version.js
+++
@@ -1,6 +0,0 @@
-// Free Version
-var sversion = { "present":"1.0.0 SS",
-                "update" : " 1.2.0 SS" } ;
-// Premium Version
-var pversion = { "present":"1.0.0 PS" ,
-                 "update":" " }
